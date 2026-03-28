ee4f993cf46ced07e116152f34bcd1ca8f667267
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2026-03-28 21:57:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Profile from './components/Profile';
+
+const App = () => {
+  return (
+    <div className='max-w-173 mx-auto '>
+      <Profile />
+    </div>
+  )
+}
+
+export default App;
+
+//  portfolio link* - https://ramx.in/
+//  portfolio link - https://siddz.com/
+//  portfolio link - https://atharvaxdevs.xyz
+//  portfolio link - https://www.69ftw.site/   
+
+//  portfolio link* - https://ramx.in/
+//  portfolio link - https://siddz.com/
+//  portfolio link - https://atharvaxdevs.xyz
+//  portfolio link - https://www.69ftw.site/   
